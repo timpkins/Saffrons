c39f01605f486f67a6555b8d79cbb016e1e4a6fd
timpkins <timpkins@163.com>
2018-07-27 18:08:42 +0800

    沉浸式状态栏

--- Pigment/src/main/java/cn/saffrons/activity/MainActivity.java
+++ Pigment/src/main/java/cn/saffrons/activity/MainActivity.java
@@ -1,11 +1,11 @@
 package cn.saffrons.activity;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import cn.saffron.activity.SaffronBaseActivity;
 import cn.saffrons.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SaffronBaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
